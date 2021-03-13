432d7cd586e968ee4715192da01d70e934a0951e
ramonarmen96 <ramonarmen96@gmail.com>
2021-03-12 17:30:15 -0700

    Stub boot/mvc, link jpa to boot dependency

---
+++ PokemonCrud/src/main/java/com/skilldistillery/pokemon/PokemonCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCrudApplication.class, args);
+	}
+
+}
